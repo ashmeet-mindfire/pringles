14cd8cf88d0acf57659405cc39cd815166fe4f16
ashmeet-mindfire <mfsi.ashmeetm@gmail.com>
2024-10-03 19:37:27 +0530

    feat: Add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,76 @@
-import Image from "next/image";
-
+/* eslint-disable @next/next/no-img-element */
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="w-full relative">
+      {/* Logo */}
+      <div className="fixed h-32 w-32 top-0 left-10 z-50">
+        <img className="h-full w-full object-contain" src="/assets/images/logo.png" alt="logo" />
+      </div>
+      {/* MenuIcon */}
+      <div className="fixed h-32 w-32 top-0 right-10 z-50">
+        <img className="h-full w-full object-contain" src="/assets/images/menu-icon.png" alt="menu-icon" />
+      </div>
+      {/* Sky */}
+      <div className="fixed top-0 left-0 h-screen w-full -z-10">
+        <img className="h-full w-full object-cover" src="/assets/images/sky.png" alt="sky" />
+      </div>
+      {/* Greenery */}
+      <div className="fixed bottom-0 w-full">
+        <img className="h-full w-full object-cover" src="/assets/images/greenery.png" alt="greenery" />
+      </div>
+      {/* Pringles */}
+      <div className="z-20 fixed bottom-6 left-1/2 -translate-x-1/2 flex items-end">
+        <div className="h-[470px] -mr-8 blue-pringles-animation">
+          <img className="h-full w-full object-contain" src="/assets/images/blue-pringles.png" alt="blue-pringles" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="h-[500px] z-10 green-pringles-animation">
+          <img className="h-full w-full object-contain" src="/assets/images/green-pringles.png" alt="green-pringles" />
+        </div>
+        <div className="h-[470px] -ml-4 orange-pringles-animation">
+          <img className="h-full w-full object-contain" src="/assets/images/orange-pringles.png" alt="orange-pringles" />
+        </div>
+      </div>
+      <div className="relative h-screen">
+        <div className="z-20 absolute bottom-0 left-[32%] h-48 w-48">
+          <img className="h-full w-full object-cover" src="/assets/images/onions.png" alt="onions" />
+        </div>
+        <div className="z-20 absolute bottom-0 right-[32%] h-52 w-72">
+          <img className="h-full w-full object-contain" src="/assets/images/chips.png" alt="chips" />
+        </div>
+        <div className="z-10 absolute -bottom-2 left-[49%] -translate-x-1/2">
+          <img className="h-full object-cover" src="/assets/images/board.png" alt="board" />
+        </div>
+      </div>
+      <div className="absolute top-[100vh] h-screen z-20 bg-green-200 w-full"></div>
+      <div className="absolute top-[100vh] h-screen z-20 left-1/2 -translate-x-1/2">
+        <img className="h-full w-full object-contain" src="/assets/images/bg-chips.png" alt="bg-chips" />
+      </div>
+      <div className="absolute top-[200vh] h-screen z-30 bg-blue-200 w-full"></div>
+      <div className="absolute top-[200vh] h-screen z-30 left-1/2 -translate-x-1/2">
+        <img className="h-full w-full object-contain" src="/assets/images/bg-chips.png" alt="bg-chips" />
+      </div>
+      <div className="absolute top-[300vh] h-screen z-40 bg-orange-200 w-full"></div>
+      <div className="absolute top-[300vh] h-screen z-40 left-1/2 -translate-x-1/2">
+        <img className="h-full w-full object-contain" src="/assets/images/bg-chips.png" alt="bg-chips" />
+      </div>
+      <div className="h-screen z-20 sticky top-0 flex items-center justify-center">
+        <p className="text-8xl font-black text-green-600 z-40">Sour Cream & Onion</p>
+        <div className="absolute left-1/2 -translate-x-1/2 h-[500px] z-40">
+          <img className="h-full w-full object-contain" src="/assets/images/green-pringles.png" alt="green-pringles" />
+        </div>
+      </div>
+      <div className="h-screen z-30 sticky top-0 flex items-center justify-center">
+        <p className="text-8xl font-black text-blue-600 z-40">Cheddar Cream & Onion</p>
+        <div className="absolute left-1/2 -translate-x-1/2 h-[500px] z-40">
+          <img className="h-full w-full object-contain" src="/assets/images/blue-pringles.png" alt="blue-pringles" />
+        </div>
+      </div>
+      <div className="h-screen z-40 sticky top-0 flex items-center justify-center">
+        <p className="text-8xl font-black text-orange-600 z-40">Cheddar Cheese</p>
+        <div className="absolute left-1/2 -translate-x-1/2 h-[500px] z-40">
+          <img className="h-full w-full object-contain" src="/assets/images/orange-pringles.png" alt="orange-pringles" />
+        </div>
+      </div>
     </div>
   );
 }
